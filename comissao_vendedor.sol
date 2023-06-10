9e4319bf59cee487162cbf56a0742053790c2d67
Diogo0602x <diogoteixeira0602@gmail.com>
2023-06-10 09:36:41 -0300

    first commit

---
+++ comissao_vendedor.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract ComissaoVendedor {
+
+    // Estrutura para armazenar os dados do vendedor
+    struct Vendedor {
+        string nome;
+        uint256 valorVenda;
+        uint256 fatorBonus;
+    }
+
+    // Mapeamento para armazenar vendedores pelo endereço Ethereum
+    mapping(address => Vendedor) public vendedores;
+
+    // Função para definir as variáveis
+    function setDados(string memory _nome, uint256 _valorVenda, uint256 _fatorBonus) public {
+        // Armazenar as informações do vendedor no mapeamento
+        vendedores[msg.sender] = Vendedor(_nome, _valorVenda, _fatorBonus);
+    }
+
+    // Função que calcula o bônus do vendedor
+    function calculoBonus(address vendedorAddress) public view returns (uint256) {
+        // Verificar se o vendedor existe
+        require(bytes(vendedores[vendedorAddress].nome).length > 0, "Vendedor não existe!");
+
+        // Calcular o bônus
+        uint256 bonus = vendedores[vendedorAddress].valorVenda * vendedores[vendedorAddress].fatorBonus / 100;
+        return bonus;
+    }
+}
