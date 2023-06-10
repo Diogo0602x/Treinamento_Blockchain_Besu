34e82a074be77a9ce8044796ddca3fc44a19207e
Diogo0602x <diogoteixeira0602@gmail.com>
2023-06-10 15:24:46 -0300

    owner and reajusted rent added

--- contrato_aluguel.sol
+++ contrato_aluguel.sol
@@ -5,13 +5,25 @@ pragma solidity 0.8.20;
 
 contract ContratoAluguel {
 
+    address public dono;
+
+    event NomeAlterado(uint8 tipoPessoa, string novoNome);
+    event AluguelReajustado(uint256 mes, uint256 aumento);
+
   string public locador;
   string public locatario;
   uint256[36] public valoresAluguel;
 
+    // Modificador para restringir o acesso ao dono do contrato
+    modifier unicoDono {
+      require(msg.sender == dono, "Somente o dono do contrato pode realizar essa operação.");
+      _;
+    }
+
   constructor(string memory _locador, string memory _locatario, uint256 valorInicialAluguel) {
     locador = _locador;
     locatario = _locatario;
+    dono = msg.sender;
 
     for (uint i = 0; i < 36; i++) {
       valoresAluguel[i] = valorInicialAluguel;
@@ -24,7 +36,7 @@ contract ContratoAluguel {
   }
 
   function getNomes() public view returns (string memory, string memory) {
-      return (locador, locatario);
+    return (locador, locatario);
   }
 
   function alterarNome(uint8 tipoPessoa, string memory novoNome) public {
@@ -35,13 +47,17 @@ contract ContratoAluguel {
     } else {
       revert("Tipo de pessoa inválido. Use 1 para locador e 2 para locatário.");
     }
+
+    emit NomeAlterado(tipoPessoa, novoNome);
   }
 
-  function reajusteAluguel(uint256 mes, uint256 aumento) public {
+  function reajusteAluguel(uint256 mes, uint256 aumento) public onlyOwner {
     require(mes > 0 && mes <= 36, "Mês inválido. Insira um valor entre 1 e 36.");
 
     for (uint i = mes; i < 36; i++) {
       valoresAluguel[i] += aumento;
     }
+
+    emit AluguelReajustado(mes, aumento);
   }
 }
